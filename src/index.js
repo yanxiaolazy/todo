d33698ace8d2ce5b09f82e3cd30647747c646fb7
Li <53163146+yanxiaolazy@users.noreply.github.com>
2021-02-05 14:46:51 +0800

    update(index.js): update path from `./Store` to `./store`

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from "./Store";
+import store from "./store";
 import './index.less';
 import reportWebVitals from './reportWebVitals';
 
